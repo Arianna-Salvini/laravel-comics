2821ca709bfc747f8912d9e26fe241b1c9d65c26
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-05-08 15:48:04 +0200

    Add fontosom and font  stle links in head

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,6 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel Comics </title>
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap"
+        rel="stylesheet">
+
+
     <!-- Styles -->
     @vite('resources/js/app.js')
 
@@ -19,6 +25,8 @@
     </main>
 
     @include('partials.footer')
+
+    <script src="https://kit.fontawesome.com/ac8f8c0862.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
