9a2edce462e832cd11c5fe0c876245cd2a469d33
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-05-08 14:28:51 +0200

    Add footer top part with logos, backgrpund and liks. links titled in "Dc Comics" and "Shop" are linked to relative pages

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,152 @@
-<footer class=" p-y5 bg-dark text-light ">
-    Footer
+<footer class=" p-y5 bg-dark text-light">
+    <div class="top_footer">
+        <div class="container h-100 d-flex justify-content-between">
+            <div class="footer_top_right d-flex justify-content-between align-items-center">
+                {{--  LIKS  --}}
+                <div class="links">
+                    <div class="row p-2">
+                        <div class="col-4">
+                            {{--  ADD DC COMICS LINK  --}}
+                            <ul class="list-unstyled">
+                                <h5 class="mb-0">DC COMICS</h5>
+                                <li>
+                                    <a href="{{ route('characters') }}"
+                                        class="link-light text-decoration-none fw-lighter">Characters</a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('comics') }}"
+                                        class="link-light text-decoration-none fw-lighter">Comics</a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('movies') }}"
+                                        class="link-light text-decoration-none fw-lighter">Movies</a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('tv') }}"
+                                        class="link-light text-decoration-none fw-lighter">TV</a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('games') }}"
+                                        class="link-light text-decoration-none fw-lighter">Games</a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('collectibles') }}"
+                                        class="link-light text-decoration-none fw-lighter">Collectibles</a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('videos') }}"
+                                        class="link-light text-decoration-none fw-lighter">Videos</a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('fans') }}"
+                                        class="link-light text-decoration-none fw-lighter">Fans</a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('news') }}"
+                                        class="link-light text-decoration-none fw-lighter">News</a>
+                                </li>
+
+                            </ul>
+
+                            {{--  ADD SHOP LINK  --}}
+                            <ul class="list-unstyled">
+                                <h5 class="mb-0">SHOP</h5>
+                                <li>
+                                    <a href="{{ route('shop') }}" class="link-light text-decoration-none fw-lighter">DC
+                                        Shop</a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('shop') }}" class="link-light text-decoration-none fw-lighter">DC
+                                        Shop
+                                        Collectabiles</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-4">
+                            {{--  ADD DC LINK  --}}
+                            <ul class="list-unstyled">
+                                <h5 class="mb-0">DC</h5>
+                                <li>
+                                    <a href="#" class="link-light text-decoration-none fw-lighter"> Terms Of
+                                        Using</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="link-light text-decoration-none fw-lighter"> Privacy
+                                        policy</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="link-light text-decoration-none fw-lighter">Ad Choices</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="link-light text-decoration-none fw-lighter">Advertising</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="link-light text-decoration-none fw-lighter">Job</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                        class="link-light text-decoration-none fw-lighter">Subscription</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="link-light text-decoration-none fw-lighter">Talent
+                                        Workshops</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="link-light text-decoration-none fw-lighter">CPSC
+                                        Certificates</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="link-light text-decoration-none fw-lighter">Ratings</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="link-light text-decoration-none fw-lighter">Shop Help</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="link-light text-decoration-none fw-lighter">Contact Us</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-4">
+                            {{--  ADD SITES LINK  --}}
+                            <ul class="list-unstyled">
+                                <h5 class="mb-0">SITES</h5>
+                                <li>
+                                    <a href="#" class="link-light text-decoration-none fw-lighter">DC</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="link-light text-decoration-none fw-lighter">MAD MAGAZINE
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="link-light text-decoration-none fw-lighter">DC Kids</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="link-light text-decoration-none fw-lighter">DC Universe</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="link-light text-decoration-none fw-lighter">DC Power Of
+                                        Visa</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-12">
+                            <p class="ink-light text-decoration-none fw-lighter mb-0">All Site Content TM and DC
+                                Entertainment,
+                                Unless otherwise <a href="#" class="text-decoration-none fw-normal m-0">noted
+                                    here</a>.
+                                All
+                                rights reserved.</p>
+                            <a href="#" class="text-decoration-none fw-normal mt-0">Cookies Settings</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer_top_right  d-flex justify-content-end align-items-center">
+                <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="">
+            </div>
+        </div>
+
+    </div>
+    <div class="bottom_footer">
+    </div>
 </footer>
