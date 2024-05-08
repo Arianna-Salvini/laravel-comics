ca19127d050c3bba93213f54b646e2fbdaa2fa2b
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-05-08 11:46:54 +0200

    Fix comics page

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,21 +1,21 @@
 @extends('layout.app')
 
 @section('content')
-<div class="bg-dark">
-    <div class="container position-relative">
-        <span class="label ">CURRENT SERIES</span>
-        <div class="row row-cols-1 row-cols-md-3 row-cols-lg-6 g-4  current_series_box">
-            @foreach ($db_comics as $db_comic)
-            <div class="col">
-                <div class="card comic_card">
-                    <img class="card-img-top card_top" src="{{$db_comic['thumb']}}" alt="">
-                    <div class="card-body bg-dark text-light border-dark card_text">
-                        {{$db_comic['title']}}
+    <div class="bg-dark">
+        <div class="container position-relative">
+            <span class="label ">CURRENT SERIES</span>
+            <div class="row row-cols-1 row-cols-md-3 row-cols-lg-6 g-4  current_series_box">
+                @foreach ($db_comics as $db_comic)
+                    <div class="col">
+                        <div class="card comic_card">
+                            <img class="card-img-top card_top" src="{{ $db_comic['thumb'] }}" alt="">
+                            <div class="card-body bg-dark text-light border-dark card_text">
+                                {{ $db_comic['title'] }}
+                            </div>
+                        </div>
                     </div>
-                </div>
+                @endforeach
             </div>
-            @endforeach
-        </div>
 
-    </div>
+        </div>
     @endsection
