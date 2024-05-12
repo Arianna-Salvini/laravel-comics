9018e07545b9f69daaa85a86cbbefa5f970e1fc8
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-05-12 18:50:54 +0200

    Add section for more details at bottom of main section

--- resources/views/single_comic/single_page_comic.blade.php
+++ resources/views/single_comic/single_page_comic.blade.php
@@ -32,8 +32,42 @@
                 <img src="{{ Vite::asset('resources/img/adv.jpg') }}" alt="">
             </div>
         </div>
-        <div class="div">
-
-        </div>
     </div>
-@endsection
+    <div class="specific_details bg-light">
+        <div class="container d-flex justify-content-between">
+            <div class="talent ps-5 flex-grow-1 specific_details_box left">
+                <div class="border-bottom">
+                    <h5 class="text-uppercase comic_primary_dark p-3">talent</h5>
+                </div>
+                <div class="border-bottom d-flex">
+                    <div class="comic_primary_dark p-3 pe-5">Art by:</div>
+                    <a href="#"
+                        class="comic_primary_light p-3 flex-grow-1 text-decoration-none">{{ $comic['artists'] }}</a>
+                </div>
+                <div class="border-bottom d-flex">
+                    <div class="comic_primary_dark p-3  ">Written by:</div>
+                    <a href="#"
+                        class="comic_primary_light p-3 flex-grow-1 text-decoration-none">{{ $comic['writers'] }}</a>
+                </div>
+            </div>
+            <div class="specs px-5 flex-grow-1 specific_details_box right">
+                <div class="border-bottom">
+                    <h5 class="text-uppercase comic_primary_dark p-3">specs</h5>
+                </div>
+                <div class="border-bottom d-flex">
+                    <div class="comic_primary_dark p-3 ">Series:</div>
+                    <div class="comic_primary_light p-3 flex-grow-1">{{ $comic['series'] }}</div>
+                </div>
+                <div class="border-bottom d-flex">
+                    <div class="comic_primary_dark p-3 ">U.S. Price:</div>
+                    <div class="comic_primary_light p-3 flex-grow-1">{{ $comic['price'] }}</div>
+
+                </div>
+                <div class="border-bottom d-flex">
+                    <div class="comic_primary_dark p-3 ">On Sale Date:</div>
+                    <div class="comic_primary_light p-3 flex-grow-1">{{ $comic['sale_date'] }}</div>
+
+                </div>
+            </div>
+        </div>
+    @endsection
